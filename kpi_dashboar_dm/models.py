f5ae6e4cc260659179f7477d2199725247055933
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-31 18:55:34 +0000

    Update models

--- kpi_dashboar_dm/models.py
+++ kpi_dashboar_dm/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Article(models.Model):
     'Generated Model'
     title = models.BigIntegerField()
+    description = models.CharField(max_length=256,null=True,blank=True,)
 
 # Create your models here.
